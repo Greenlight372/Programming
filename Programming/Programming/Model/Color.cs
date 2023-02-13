f2a62c623a98ea6e75507b86caf74de76c734d2b
Greenlight372 <dimgreen372@gmail.com>
2023-02-13 14:52:03 +0700

    Added a bunch of other stuff for lab number 2

---
+++ Programming/Programming/Model/Color.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming.Model
+{
+    public enum Color
+    {
+        Red,
+        Green,
+        Blue,
+        Purple,
+        Yellow,
+        Cyan,
+        Grey,
+        Black,
+        White
+    }
+}
