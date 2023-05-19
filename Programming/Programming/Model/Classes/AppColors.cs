b151711c017016c0c78b11463e9428878a2ec298
Greenlight372 <dimgreen372@gmail.com>
2023-05-19 19:23:51 +0700

    Some changes.

--- Programming/Programming/Model/Classes/AppColors.cs
+++ Programming/Programming/Model/Classes/AppColors.cs
@@ -14,36 +14,36 @@ namespace Programming.Model.Classes
         /// <summary>
         /// Красный.
         /// </summary>
-        public static System.Drawing.Color Red = System.Drawing.Color.FromArgb(127, 255, 127, 127);
+        public static Color Red = Color.FromArgb(127, 255, 127, 127);
         
         /// <summary>
         /// Зеленый.
         /// </summary>
-        public static System.Drawing.Color Green = System.Drawing.Color.FromArgb(127, 127, 255, 127);
+        public static Color Green = Color.FromArgb(127, 127, 255, 127);
 
         /// <summary>
         /// Черный с прозрачностью 50%.
         /// </summary>
-        public static System.Drawing.Color TransparentBlack_50 = System.Drawing.Color.FromArgb(127, 0, 0, 0);
+        public static Color TransparentBlack_50 = Color.FromArgb(127, 0, 0, 0);
         
         /// <summary>
         /// Черный с прозрачностью 25%.
         /// </summary>
-        public static System.Drawing.Color TransparentBlack_25 = System.Drawing.Color.FromArgb(63, 0, 0, 0);
+        public static Color TransparentBlack_25 = Color.FromArgb(63, 0, 0, 0);
 
         /// <summary>
         /// Светло-розовый.
         /// </summary>
-        public static System.Drawing.Color LightPink = System.Drawing.Color.LightPink;
+        public static Color LightPink = Color.LightPink;
         
         /// <summary>
         /// Белый.
         /// </summary>
-        public static System.Drawing.Color White = System.Drawing.Color.White;
+        public static Color White = Color.White;
         
         /// <summary>
         /// Оранжевый.
         /// </summary>
-        public static System.Drawing.Color Orange = System.Drawing.Color.Orange;
+        public static Color Orange = Color.Orange;
     }
 }
